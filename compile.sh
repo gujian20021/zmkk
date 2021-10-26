3e99627b61116438bbea1f9cdf8cf395c5b57ca5
matt.cfosse@gmail.com <matt.cfosse@gmail.com>
2021-10-26 16:21:52 -0400

    test build

--- compile.sh
+++
@@ -1,1 +0,0 @@
-python -m west build -B="C:/Users/codexuser/projects/zmk-config/build" -d build/left -b nice_nano_v2 -- -DSHIELD=corne_left -DZMK_CONFIG="C:/Users/codexuser/projects/zmk-config/config"
