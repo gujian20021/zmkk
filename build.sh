eadde068db7c73230a0ff79329fc769aa27b7ab6
fossephate <matt.cfosse@gmail.com>
2021-11-17 04:49:23 -0500

    update

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ python -m west build -s "C:/Users/fosse/projects/zmk-config-trackball/app" -b ni
 
 
 
-west build -s /workspaces/zmk-config-trackball/app -d build/left3 -b nice_nano_v2 -- -DSHIELD=corne_left -DZMK_CONFIG=/workspaces/zmk-config-trackball/zmk-config/config
-west build -s "/mnt/c/Users/fosse/projects/zmk-config-trackball/app" -d build/right -b nice_nano_v2 -- -DSHIELD=corne_right -DZMK_CONFIG="/mnt/c/Users/fosse/projects/zmk-config/config"
+west build --pristine -s /workspaces/zmk-trackball/app -d build/left -b nice_nano_v2 -- -DSHIELD=corne_left -DZMK_CONFIG=/workspaces/zmk-trackball/zmk-config/config
+west build --pristine -s /workspaces/zmk-trackball/app -d build/right -b nice_nano_v2 -- -DSHIELD=corne_right -DZMK_CONFIG=/workspaces/zmk-trackball/zmk-config/config
